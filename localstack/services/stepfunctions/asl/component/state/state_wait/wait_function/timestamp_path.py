7570ae4a27ceb8483b817aac169381d37423ab68
Dominik Schubert <dominik.schubert91@gmail.com>
2023-12-06 08:56:46 +0100

    temp state

--- localstack/services/stepfunctions/asl/component/state/state_wait/wait_function/timestamp_path.py
+++ localstack/services/stepfunctions/asl/component/state/state_wait/wait_function/timestamp_path.py
@@ -21,7 +21,23 @@ class TimestampPath(WaitFunction):
 
     def _get_wait_seconds(self, env: Environment) -> int:
         timestamp_str = JSONPathUtils.extract_json(self.path, env.inp)
-        timestamp = datetime.datetime.strptime(timestamp_str, Timestamp.TIMESTAMP_FORMAT)
+        try:
+            timestamp = datetime.datetime.strptime(timestamp_str, Timestamp.TIMESTAMP_FORMAT)
+        except Exception:
+            raise
+            # report_error(f"The TimestampPath parameter does not reference a valid ISO-8601 extended offset date-time format string: {self.path} == {timestamp_str}")
+            # state_name = env.context_object_manager.context_object["State"]["Name"]
+            # env.event_history_context.source_event_id
+            # raise FailureEventException(FailureEvent(
+            #     error_name=StatesErrorName(typ=StatesErrorNameType.StatesRuntime),
+            #     event_type=HistoryEventType.ExecutionFailed,
+            #     event_details=EventDetails(
+            #         executionFailedEventDetails=ExecutionFailedEventDetails(
+            #             error="States.Runtime",
+            #             cause=f"The TimestampPath parameter does not reference a valid ISO-8601 extended offset date-time format string: {self.path} == {timestamp_str}"
+            #         )
+            #     )
+            # ))
         delta = timestamp - datetime.datetime.today()
         delta_sec = int(delta.total_seconds())
         return delta_sec
